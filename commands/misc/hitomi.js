6d92c2bdedc81f63e0ecdef84f7e447c515f9f0d
leehyowon14 <kingtv347@gmail.com>
2021-02-11 22:49:25 +0900

    에러메세지 추가 & 작동 안되던 오류수정

--- commands/misc/hitomi.js
+++ commands/misc/hitomi.js
@@ -11,54 +11,69 @@ module.exports = {
         accessableby: "Members",
     },
     run: async (bot, message, args) => {
-        try{
-            let arg = message.content.split(" ")
-            const number = arg[1]
-
-            var url = `https://ltn.hitomi.la/galleryblock/${number}.html`;
-            request(url, function(error, response, html){
-                if (error) {throw error};
-                
-                var $ = cheerio.load(html);
+        const number = args[0]
+        if (!number) {
+            let embed = new MessageEmbed()
+            .setColor('#f94343')
+            .setAuthor('에러!')
+            .setTitle('번호를 입력해주세요')
+            .setTimestamp()
+            .setFooter('Developed by 느윽대#5070')
         
-                const title = $("h1.lillie > a").text()
+        message.channel.send(embed)
+        }
 
+        
+        var url = `https://ltn.hitomi.la/galleryblock/${number}.html`;
+
+        request(url, function(error, response, html){
+                
+            var $ = cheerio.load(html);
+        
+            const title = $("h1.lillie > a").text()
             
-                const tags = $(".relatedtags a").text()
+            if (!title) {
+                let embed = new MessageEmbed()
+                    .setColor('#f94343')
+                    .setAuthor('에러!')
+                    .setTitle('없는작품입니다')
+                    .setTimestamp()
+                    .setFooter('Developed by 느윽대#5070')
+                message.channel.send(embed)
+            }
+            
+            const tags = $(".relatedtags a").text()
 
-                const languages = $("a[href~=^/index.+\\.html\$]").text()
-                if (languages == "한국어") {
-                    let embed = new MessageEmbed()
-                        .setColor('#73c4fa')
-                        .setTitle('HITOMI(HIYOBI) HELPER')
-                        .addField('제목', `${title}`)
-                        .addField('언어', `${languages}`)
-                        .addField('히토미 링크', `https://hitomi.la/galleries/${number}.html`)
-                        .addField('히요비 링크', `https://hiyobi.me/reader/${number}`)
-                        .addField('태그', `${tags}`)
-                        .addField('\u200B', '\u200B')
-                        .setTimestamp()
-                        .setFooter('Developed by 느윽대#5070')
+            const languages = $("a[href^='/index'][href$='.html']").text()
 
-                    message.channel.send(embed)
-                }else{
-                    let embed = new MessageEmbed()
-                        .setColor('#73c4fa')
-                        .setTitle('HITOMI(HIYOBI) HELPER')
-                        .addField('제목', `${title}`)
-                        .addField('언어', `${languages}`)
-                        .addField('히토미 링크', `https://hitomi.la/galleries/${number}.html`)
-                        .addField('태그', `${tags}`)
-                        .addField('\u200B', '\u200B')
-                        .setTimestamp()
-                        .setFooter('Developed by 느윽대#5070')
+            if (languages == "한국어") {
+                let embed = new MessageEmbed()
+                    .setColor('#73c4fa')
+                    .setTitle('HITOMI(HIYOBI) HELPER')
+                    .addField('제목', `${title}`)
+                    .addField('언어', languages)
+                    .addField('히토미 링크', `https://hitomi.la/galleries/${number}.html`)
+                    .addField('히요비 링크', `https://hiyobi.me/reader/${number}`)
+                    .addField('태그', `${tags}`)
+                    .addField('\u200B', '\u200B')
+                    .setTimestamp()
+                    .setFooter('Developed by 느윽대#5070')
 
-                    message.channel.send(embed)
+                message.channel.send(embed)
+            }else{
+                let embed = new MessageEmbed()
+                    .setColor('#73c4fa')
+                    .setTitle('HITOMI HELPER')
+                    .addField('제목', `${title}`)
+                    .addField('언어', languages)
+                    .addField('히토미 링크', `https://hitomi.la/galleries/${number}.html`)
+                    .addField('태그', `${tags}`)
+                    .addField('\u200B', '\u200B')
+                    .setTimestamp()
+                    .setFooter('Developed by 느윽대#5070')
+
+                message.channel.send(embed)
                 }
             }); 
-            } catch (e) {
-                console.log(e);
-                return webhook.send(`An error occurred: **${e.message}**`);
-            }
     }
 }
