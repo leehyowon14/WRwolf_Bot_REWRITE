67026505a2a9936d64ea76217d77bf24cf719159
leehyowon14 <kingtv347@gmail.com>
2021-02-17 23:24:26 +0900

    메세지 추가/쿨 빠르게 변경

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,9 +3,10 @@ module.exports = bot => {
         `${bot.guilds.cache.size} servers!`,
         `${bot.channels.cache.size} channels!`,
         `${bot.users.cache.size} users!`
+        `버그제보/각종 문의는 봇 DM으로!`
     ];
     let i = 0;
-    setInterval(() => bot.user.setActivity(`${prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000)
+    setInterval(() => bot.user.setActivity(`${prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }), 5000)
 
     log(`${redChalk(bot.user.username)} ${greenChalk('is online')}`);
 };
