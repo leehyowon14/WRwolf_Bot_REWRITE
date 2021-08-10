f8636f66fd94fab9ae4890917bfe56111755151d
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2021-08-10 13:10:28 +0900

    청소커맨드로 삭제한 메세지들 로그 추가

---
+++ events/guild/messageDeleteBulk.js
@@ -0,0 +1,39 @@
+const { MessageAttachment } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
+const dateFormat = require('dateformat')
+
+module.exports = async (bot, messages) => {
+    if (!messages.first().guild.systemChannel) {
+    return;
+    }
+    let embed = new MessageEmbed()
+        .setTitle('Chatting Log')
+        .setColor('#5865F2')
+        .addField('Log-Type', 'Deleted Message(Bulk)')
+        //.addField('Message By:', message.author.tag)
+        .addField('Channel:', messages.first().channel.name)
+        //.setFooter(message.author.tag)
+        .setTimestamp()
+        messages.first().guild.systemChannel.send({ embeds: [embed] })
+
+        let Log = `Deleted Messages`;
+        for (const message of [...messages.values()].reverse()) {
+            Log += `\r\n\r\n[${dateFormat(message.createdAt, 'yyyy/mm/dd ddd HH:MM:ss')}] ${message.author?.tag ?? 'Unknown'} `;
+            if([...message.attachments.values()].length > 0) {
+                if(message.content.length == 0) {
+                    for (let i = 0; i<[...message.attachments.values()].length; i++) {
+                        Log += '\r\n[attachment] :' + [...message.attachments.values()][i].url;
+                    }
+                } else {
+                    Log += ' : ' + message.content;
+                    for (let i = 0; i<[...message.attachments.values()].length; i++) {
+                        Log += '\r\n[attachment] :' + [...message.attachments.values()][i].url;
+                    }
+                }
+            }
+            Log += ' : ' + message.content;
+        }
+
+        const log_txt_file = new MessageAttachment(Buffer.from(Log, 'utf-8'), 'DeletedMessages.txt');
+        messages.first().guild.systemChannel.send({ files : [log_txt_file] });
+}
