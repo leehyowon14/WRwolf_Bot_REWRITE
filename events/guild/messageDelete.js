99b54f6563160baa4e04686215f9304cdda5cbcf
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2021-08-10 17:06:16 +0900

    버그 수정(.js/.ico와같은 파일 올릴시 봇 꺼지는 버그)

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -10,11 +10,6 @@ module.exports = async (bot, message) => {
     }
     if([...message.attachments.values()].length > 0) {
         try {
-            const result = await fetch([...message.attachments.values()][0].proxyURL);
-            if (!result.ok) throw new Error('Failed to get the avatar!');
-            const avatar = await result.buffer();
-
-            const attachment = new MessageAttachment(avatar, [...message.attachments.values()][0].name);
             if(message.content.length == 0) {
                 let img = message.author.avatar ? `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.webp?size=256` : undefined;
                 let embed = new MessageEmbed()
@@ -41,10 +36,13 @@ module.exports = async (bot, message) => {
                 
                     message.guild.systemChannel.send({ embeds: [embed] })
             }
-            return message.guild.systemChannel.send({ files : [attachment] });
+            for (let i = 0; i<[...message.attachments.values()].length; i++) {
+                message.guild.systemChannel.send({ content : [...message.attachments.values()][i].url });
+              }
+            return;
         } catch (e) {
             console.log(e);
-            return message.guild.SystemChannel.send(`An error occurred: **${e.message}**`);
+            return message.guild.SystemChannel.send({content: `An error occurred: **${e.message}**` });
         }
     }
     let img = message.author.avatar ? `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.webp?size=256` : undefined;
