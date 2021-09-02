b3c4ac2f84411aa7b060b8ef8199abd9dea259c7
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2021-09-02 20:45:59 +0900

    hotfix! improve nsfw help

--- commands/misc/nsfw_help.js
+++ commands/misc/nsfw_help.js
@@ -43,6 +43,7 @@ module.exports = {
             .addField('\u200B', `${prefix}keta`, true)
             .addField('\u200B', `${prefix}kitsune`, true)
         let embed2 = new MessageEmbed()
+            .setColor('#5865F2')
             .addField('\u200B', `${prefix}kuni`, true)
             .addField('\u200B', `${prefix}lesbian`, true)
             .addField('\u200B', `${prefix}lewdavatar`, true)
