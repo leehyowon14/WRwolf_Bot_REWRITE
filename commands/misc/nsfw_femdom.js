5c86ab58d4e669b9860b0ab654a3721e8217a134
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2021-09-02 20:07:25 +0900

    nsfw feature

---
+++ commands/misc/nsfw_femdom.js
@@ -0,0 +1,38 @@
+const client = require('nekos.life');
+const Discord = require('discord.js')
+const neko = new client();
+
+module.exports = {
+    config: {
+        name: `femdom`,
+        aliases: [`${prefix}femdom`],
+        description: "",
+        usage: "",
+        accessableby: "Members",
+    },
+    run: async (bot, message, args) => {
+  //command
+
+  //Checks channel for nsfw
+   
+  if (!message.channel.nsfw) {
+      message.react('💢');
+
+      return message.reply({ content : `이곳은 NSFW채널이 아닙니다.`, allowedMentions: {repliedUser: true} })
+      
+  }
+
+  async function emsend() {
+    let uwu = (await neko.nsfw.femdom());
+
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Femdom")
+    .setImage(uwu.url)
+    .setColor(`#FF0000`)
+    .setURL(uwu.url);
+    message.channel.send({ embeds: [embed] });
+
+    }
+        emsend();
+    }
+};
