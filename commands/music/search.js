b8dcfe7db7fafa85cc51fbc0bfe9b76281e7e9d9
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2021-09-26 19:57:46 +0900

    improve

--- commands/music/search.js
+++ commands/music/search.js
@@ -1,5 +1,13 @@
 const request = require("request")
 const { MessageEmbed } = require("discord.js");
+
+function tinyurl(url) {
+    request("https://tinyurl.com/api-create.php?url=" + url, (error, response, body) => {
+        if (body == "Error") return url;
+        return body
+    })
+}
+
 module.exports = {
     config: {
         name: `searchmusic`,
@@ -36,9 +44,9 @@ module.exports = {
             let artist_1 = data.tracks.items[0].album.artists[0].name
             let album_1 = data.tracks.items[0].album.name
             let album_type_1 = data.tracks.items[0].album.album_type
-            let song_url_1 = data.tracks.items[0].external_urls.spotify
+            let song_url_1 = tinyurl(data.tracks.items[0].external_urls.spotify)
             let song_name_1 = data.tracks.items[0].name
-            let song_preview_1 = data.tracks.items[0].preview_url
+            let song_preview_1 = tinyurl(data.tracks.items[0].preview_url)
             if (data.tracks.total == 1) {
                 let embed = new MessageEmbed()
                     .setColor('#57F287')
@@ -52,9 +60,9 @@ module.exports = {
             let artist_2 = data.tracks.items[1].album.artists[0].name
             let album_2 = data.tracks.items[1].album.name
             let album_type_2 = data.tracks.items[1].album.album_type
-            let song_url_2 = data.tracks.items[1].external_urls.spotify
+            let song_url_2 = tinyurl(data.tracks.items[1].external_urls.spotify)
             let song_name_2 = data.tracks.items[1].name
-            let song_preview_2 = data.tracks.items[1].preview_url
+            let song_preview_2 = tinyurl(data.tracks.items[1].preview_url)
             if (data.tracks.total == 2) {
                 let embed = new MessageEmbed()
                     .setColor('#57F287')
@@ -70,9 +78,9 @@ module.exports = {
             let artist_3 = data.tracks.items[2].album.artists[0].name
             let album_3 = data.tracks.items[2].album.name
             let album_type_3 = data.tracks.items[2].album.album_type
-            let song_url_3 = data.tracks.items[2].external_urls.spotify
+            let song_url_3 = tinyurl(data.tracks.items[2].external_urls.spotify)
             let song_name_3 = data.tracks.items[2].name
-            let song_preview_3 = data.tracks.items[2].preview_url
+            let song_preview_3 = tinyurl(data.tracks.items[2].preview_url)
 
             let embed = new MessageEmbed()
                 .setColor('#57F287')
