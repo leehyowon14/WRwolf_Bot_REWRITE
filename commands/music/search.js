8cd157b61cc371588103d6c6eff80b7ad1e03b92
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2021-09-27 13:47:30 +0900

    뷁

--- commands/music/search.js
+++ commands/music/search.js
@@ -48,9 +48,13 @@ module.exports = {
             let artist_1 = data.tracks.items[0].album.artists[0].name
             let album_1 = data.tracks.items[0].album.name
             let album_type_1 = data.tracks.items[0].album.album_type
-            let song_url_1 = tinyurl.run(data.tracks.items[0].external_urls.spotify).then(v => song_url_1 = v)
+            let song_url_1 = tinyurl.run(data.tracks.items[0].external_urls.spotify).then(function (v) {
+                song_url_1 = v
+            })
             let song_name_1 = data.tracks.items[0].name
-            let song_preview_1 = tinyurl.run(data.tracks.items[0].preview_url).then(v => song_preview_1 = v)
+            let song_preview_1 = tinyurl.run(data.tracks.items[0].preview_url).then(function (v) {
+                song_preview_1 = v
+            })
             if (data.tracks.total == 1) {
                 let embed = new MessageEmbed()
                     .setColor('#57F287')
@@ -64,9 +68,13 @@ module.exports = {
             let artist_2 = data.tracks.items[1].album.artists[0].name
             let album_2 = data.tracks.items[1].album.name
             let album_type_2 = data.tracks.items[1].album.album_type
-            let song_url_2 = tinyurl.run(data.tracks.items[1].external_urls.spotify).then(v => song_url_2 = v)
+            let song_url_2 = tinyurl.run(data.tracks.items[1].external_urls.spotify).then(function (v) {
+                song_url_2 = v
+            })
             let song_name_2 = data.tracks.items[1].name
-            let song_preview_2 = tinyurl.run(data.tracks.items[1].preview_url).then(v => song_preview_2 = v)
+            let song_preview_2 = tinyurl.run(data.tracks.items[1].preview_url).then(function (v) {
+                song_preview_2 = v
+            })
             if (data.tracks.total == 2) {
                 let embed = new MessageEmbed()
                     .setColor('#57F287')
@@ -82,9 +90,13 @@ module.exports = {
             let artist_3 = data.tracks.items[2].album.artists[0].name
             let album_3 = data.tracks.items[2].album.name
             let album_type_3 = data.tracks.items[2].album.album_type
-            let song_url_3 = tinyurl.run(data.tracks.items[2].external_urls.spotify).then(v => song_url_3 = v)
+            let song_url_3 = tinyurl.run(data.tracks.items[2].external_urls.spotify).then(function (v) {
+                song_url_3 = v
+            })
             let song_name_3 = data.tracks.items[2].name
-            let song_preview_3 = tinyurl.run(data.tracks.items[2].preview_url).then(v => song_preview_3 = v)
+            let song_preview_3 = tinyurl.run(data.tracks.items[2].preview_url).then(function (v) {
+                song_preview_3 = v
+            })
 
             let embed = new MessageEmbed()
                 .setColor('#57F287')
