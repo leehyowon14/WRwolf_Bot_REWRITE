ad1ee8c5afbedd2482fdf9fcb72a20ad25f3e721
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2021-09-27 13:30:45 +0900

    123

--- modules/tinyurl.js
+++ modules/tinyurl.js
@@ -4,7 +4,7 @@ module.exports = {
     run: async (url) => {
         return new Promise((resolve, reject) => {
             request("https://tinyurl.com/api-create.php?url=" + url, (error, response, body) => {
-                console.log(body)
+                console.log("tinyurl module: " + body)
                 if (body == "Error") {
                     resolve(url);
                 }
