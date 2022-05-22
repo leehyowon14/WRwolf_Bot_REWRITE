9633f535f7077f3c115ac43f92df4e4c56ee4143
leehyowon14 <lhwmessageonlyemail@gmail.com>
2022-05-22 17:11:56 +0900

    카리나 버그 픽스

--- commands/non-slash/aespa/update_karina.js
+++ commands/non-slash/aespa/update_karina.js
@@ -2,32 +2,32 @@ const { MessageEmbed } = require("discord.js");
 const fs = require('fs');
 
 async function fetchAllMessages(bot) {
-    const channel = bot.channels.cache.get("937378383912927302");
-    let messages = [];
-  
-    // Create message pointer
-    let message = await channel.messages
-      .fetch({ limit: 1 })
-      .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
-  
-    while (message) {
-      await channel.messages
-        .fetch({ limit: 100, before: message.id })
-        .then(messagePage => {
-          messagePage.forEach(msg => {
-            if ([...msg.attachments.values()].length > 0) {
-              for (let i = 0; i<[...msg.attachments.values()].length; i++) {
-                messages.push([...msg.attachments.values()][i].url);
-              }
+  const channel = bot.channels.cache.get("937378383912927302");
+  let messages = [];
+
+  // Create message pointer
+  let message = await channel.messages
+    .fetch({ limit: 1 })
+    .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+
+  while (message) {
+    await channel.messages
+      .fetch({ limit: 100, before: message.id })
+      .then(messagePage => {
+        messagePage.forEach(msg => {
+          if ([...msg.attachments.values()].length > 0) {
+            for (let i = 0; i<[...msg.attachments.values()].length; i++) {
+              messages.push([...msg.attachments.values()][i].url);
             }
-          });
-  
-          // Update our message pointer to be last message in page of messages
-          message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
+          }
         });
-    }
-    console.log("get all karina picture's link successfully");
-    return messages;
+
+        // Update our message pointer to be last message in page of messages
+        message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
+      });
+  }
+  console.log("get all winter picture's link successfully");
+  return messages;
 } //https://stackoverflow.com/questions/63322284/discord-js-get-an-array-of-all-messages-in-a-channel
 
 module.exports = {
