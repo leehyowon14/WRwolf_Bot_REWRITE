97986e155cd2411679aebde2ea977c15d7e89aa6
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2022-05-26 20:30:45 +0900

    hitfix

--- commands/non-slash/nsfw/nsfw_hkitsune.js
+++ commands/non-slash/nsfw/nsfw_hkitsune.js
@@ -10,23 +10,24 @@ module.exports = {
         accessableby: "Members",
     },
     run: async (bot, message, args) => {
-  //Checks channel for nsfw
-   
-  if (!message.channel.nsfw) {
-      message.react('💢');
+        //Checks channel for nsfw
 
-      return message.reply({ content : `이곳은 NSFW채널이 아닙니다.`, allowedMentions: {repliedUser: true} })
-  }
-  let response = await axios({
-    method: 'get',
-    url: `https://nekobot.xyz/api/image?type=hkitsune`,
-  });
-  if (response.status !== 200) return message.channel.send("api 서버 오류");
-  av = response.data.message
+        if (!message.channel.nsfw) {
+            message.react('💢');
 
-  let embed = new Discord.MessageEmbed()
-    .setTitle("hentai kitsune")
-    .setImage(av)
-    .setColor(`#FF0000`)
-  message.channel.send({ embeds: [embed] });
+            return message.reply({ content : `이곳은 NSFW채널이 아닙니다.`, allowedMentions: {repliedUser: true} })
+        }
+        let response = await axios({
+            method: 'get',
+            url: `https://nekobot.xyz/api/image?type=hkitsune`,
+        });
+        if (response.status !== 200) return message.channel.send("api 서버 오류");
+        av = response.data.message
+
+        let embed = new Discord.MessageEmbed()
+            .setTitle("hentai kitsune")
+            .setImage(av)
+            .setColor(`#FF0000`)
+        message.channel.send({ embeds: [embed] });
+    }
 };
--- commands/non-slash/nsfw/nsfw_tentacle.js
+++ commands/non-slash/nsfw/nsfw_tentacle.js
@@ -10,23 +10,24 @@ module.exports = {
         accessableby: "Members",
     },
     run: async (bot, message, args) => {
-  //Checks channel for nsfw
+        //Checks channel for nsfw
    
-  if (!message.channel.nsfw) {
-      message.react('💢');
+        if (!message.channel.nsfw) {
+            message.react('💢');
 
-      return message.reply({ content : `이곳은 NSFW채널이 아닙니다.`, allowedMentions: {repliedUser: true} })
-  }
-  let response = await axios({
-    method: 'get',
-    url: `https://nekobot.xyz/api/image?type=tentacle`,
-  });
-  if (response.status !== 200) return message.channel.send("api 서버 오류");
-  av = response.data.message
+            return message.reply({ content : `이곳은 NSFW채널이 아닙니다.`, allowedMentions: {repliedUser: true} })
+        }
+        let response = await axios({
+            method: 'get',
+            url: `https://nekobot.xyz/api/image?type=tentacle`,
+        });
+        if (response.status !== 200) return message.channel.send("api 서버 오류");
+        av = response.data.message
 
-  let embed = new Discord.MessageEmbed()
-    .setTitle("tentacle")
-    .setImage(av)
-    .setColor(`#FF0000`)
-  message.channel.send({ embeds: [embed] });
+        let embed = new Discord.MessageEmbed()
+            .setTitle("tentacle")
+            .setImage(av)
+            .setColor(`#FF0000`)
+        message.channel.send({ embeds: [embed] });
+    }
 };
