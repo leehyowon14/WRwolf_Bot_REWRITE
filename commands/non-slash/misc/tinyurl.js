93deca89d2638213fc8c4cf9b131280bf952b85a
leehyowon14 <lhwmessageonlyemail@gmail.com>
2022-07-02 00:26:39 +0900

    패키지 정리 및 tinyurl 명령어 추가

---
+++ commands/non-slash/misc/tinyurl.js
@@ -0,0 +1,24 @@
+const tinyurl = require("../../../modules/tinyurl");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    config: {
+        name: "tinyurl",
+        aliases: [`${prefix}tinyurl`],
+        description: "",
+        usage: "",
+        accessableby: "Members",
+    },
+    run: async (bot, message, args) => {
+        await tinyurl.run(args[0]).then(async (url) => {
+            if (url == args[0]) url = "웹사이트 주소 단축 실패."
+            let embed = new MessageEmbed()
+                .setColor("#57F287")
+                .setTitle("TinyURL")
+                .setDescription(`${url}`)
+                .setFooter({ text: `Requested by ${message.author.tag}` })
+                .setTimestamp()
+            message.channel.send({ embeds: [embed] })
+        });
+    }
+}
