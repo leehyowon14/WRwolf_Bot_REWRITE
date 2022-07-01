26f0dc63b68484f0461004d41b79e44b85c126a5
leehyowon14 <lhwmessageonlyemail@gmail.com>
2022-07-02 00:32:00 +0900

    debug

--- commands/non-slash/misc/tinyurl.js
+++ commands/non-slash/misc/tinyurl.js
@@ -10,6 +10,7 @@ module.exports = {
         accessableby: "Members",
     },
     run: async (bot, message, args) => {
+        console.log(1)
         if (!args[0]) return message.channel.send("Please provide a link to shorten.");
         await tinyurl.run(args[0]).then(async (url) => {
             if (url == args[0]) url = "웹사이트 주소 단축 실패."
