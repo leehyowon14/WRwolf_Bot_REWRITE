59c750b5128446ce2d8e00934671af3ef68c861b
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2022-07-21 16:53:21 +0900

    rollback

--- commands/slash/tracking_number/set_Tracking_number.js
+++ commands/slash/tracking_number/set_Tracking_number.js
@@ -2,125 +2,23 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require("discord.js");
 const Tracking_number = require('../../../db/tracking_number')
 
-const delivery_org = [
-    {
-       "id": "de.dhl",
-       "name": "DHL",
-       "tel": "+8215880001"
-    },
-    {
-       "id": "kr.chunilps",
-       "name": "천일택배",
-       "tel": "+8218776606"
-    },
-    {
-       "id": "kr.cjlogistics",
-       "name": "CJ대한통운",
-       "tel": "+8215881255"
-    },
-    {
-       "id": "kr.cupost",
-       "name": "CU 편의점택배",
-       "tel": "+8215771287"
-    },
-    {
-       "id": "kr.cvsnet",
-       "name": "GS Postbox 택배",
-       "tel": "+8215771287"
-    },
-    {
-       "id": "kr.cway",
-       "name": "CWAY (Woori Express)",
-       "tel": "+8215884899"
-    },
-    {
-       "id": "kr.daesin",
-       "name": "대신택배",
-       "tel": "+82314620100"
-    },
-    {
-       "id": "kr.epost",
-       "name": "우체국 택배",
-       "tel": "+8215881300"
-    },
-    {
-       "id": "kr.hanips",
-       "name": "한의사랑택배",
-       "tel": "+8216001055"
-    },
-    {
-       "id": "kr.hanjin",
-       "name": "한진택배",
-       "tel": "+8215880011"
-    },
-    {
-       "id": "kr.hdexp",
-       "name": "합동택배",
-       "tel": "+8218993392"
-    },
-    {
-       "id": "kr.homepick",
-       "name": "홈픽",
-       "tel": "+8218000987"
-    },
-    {
-       "id": "kr.honamlogis",
-       "name": "한서호남택배",
-       "tel": "+8218770572"
-    },
-    {
-       "id": "kr.ilyanglogis",
-       "name": "일양로지스",
-       "tel": "+8215880002"
-    },
-    {
-       "id": "kr.kdexp",
-       "name": "경동택배",
-       "tel": "+8218995368"
-    },
-    {
-       "id": "kr.kunyoung",
-       "name": "건영택배",
-       "tel": "+82533543001"
-    },
-    {
-       "id": "kr.logen",
-       "name": "로젠택배",
-       "tel": "+8215889988"
-    },
-    {
-       "id": "kr.lotte",
-       "name": "롯데택배",
-       "tel": "+8215882121"
-    },
-    {
-       "id": "kr.slx",
-       "name": "SLX",
-       "tel": "+82316375400"
-    },
-    {
-       "id": "kr.swgexp",
-       "name": "성원글로벌카고",
-       "tel": "+82327469984"
-    }
- ]
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('송장번호등록')
         .setDescription('송장 번호 설정')
-        .addStringOption(option => {
+        .addStringOption(option =>
             option.setName('택배사')
                 .setDescription(
                     '택배사를 선택하여 주세요.'
-                ) 
+                )
+                .addChoice('CJ 대한통운', 'kr.cjlogistics')
+				.addChoice('롯데택배', 'kr.lotte')
+			    .addChoice('우체국 택배', 'kr.epost')
+                .addChoice('GS Postbox 택배', 'kr.cvsnet')
+                .addChoice('CU 편의점 택배', 'kr.cupost')
+                .addChoice('한진택배', 'kr.hanjin')
+                .addChoice('로젠 택배', 'kr.logen')       
                 .setRequired(true)
-                
-                for (let i = 0; i<delivery_org.length; i++) {
-                    option.addChoice(delivery_org[i].name, delivery_org[i].id);
-                    console.log(delivery_org[i].name)
-                }
-            }
         )
         .addIntegerOption(option =>
             option.setName('송장번호')
