19bcabf4b2564053d2447a9281428912b93b7f2a
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2022-07-21 12:09:40 +0900

    송장조회 기능 추가

---
+++ commands/slash/tracking_number/lookup.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
+const Tracking_number = require('../../../db/tracking_number')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('송장')
+        .setDescription('송장 번호 조회'),
+
+    async execute(interaction) {
+        let user = await Tracking_number.findOne({user_id: id})
+        if (!user) {
+            let embed = new MessageEmbed()
+                .setTitle('등록된 송장 없음!')
+                .setDescription('"/송장번호등록"을 사용하여 송장을 등록한 후에 이용해 주세요')
+                .setColor(0x00AE86)
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        } else {
+            let response = await axios({
+                method: 'get',
+                url: `https://apis.tracker.delivery/carriers/${user.org}/tracks/${user.num}/`,
+            });
+            if (response.status !== 200) return interaction.reply({content:"api 서버 오류", ephemeral: true});
+            if (!response.data.message) {
+                let data = response.data
+                let progresses = data.progresses[0]
+                let date = progresses.timedata.slice(0, 16).split('T')
+                date = date[0].split('-').concat(date[1].split(':'))
+                data = date[0]+'년 '+date[1]+'월 '+date[2]+'일 '+date[3]+'시 '+date[4]+'분'
+
+                let embed = new MessageEmbed()
+                    .setTitle("조회 성공")
+                    .setColor(0x00AE86)
+                    .setAuthor({name:`송장: ${data.carrier.name} ${user.num}`})
+                    .setDescription(`단계: ${data.state.text}\n${data.from.name} -> ${data.to.name}\n\n마지막 활동: [${date}] ${progress.status.text} 상세정보:    ${progresses.description}`)
+                
+                /*
+                단계: data.state.text
+                data.from.name -> data.to.name
+
+                마지막 활동: [date] progress.status.text
+                상세정보:    progresses.description
+                */
+                interaction.reply({ embeds: [embed], ephemeral: true });
+            } else {
+                let embed = new MessageEmbed()
+                    .setTitle('조회 실패')
+                    .setDescription(response.data.message + `\n(오류가 지속될 경우송장번호가 올바른지 확인하여 주세요)`)
+                    .setColor(0x00AE86)
+                interaction.reply({ embeds: [embed], ephemeral: true });
+            }
+        }
+    }
+}
---
+++ commands/slash/tracking_number/set_Tracking_number.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
+const Tracking_number = require('../../../db/tracking_number')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('송장번호등록')
+        .setDescription('송장 번호 설정')
+        .addStringOption(option =>
+            option.setName('택배사')
+                .addChoices(
+				    { name: 'CJ 대한통운', value: 'kr.cjlogistics' },
+				    { name: '롯데택배', value: 'kr.lotte' },
+				    { name: '우체국 택배', value: 'kr.epost' },
+                    { name: 'GS Postbox 택배', value: 'kr.cvsnet' },
+                    { name: 'CU 편의점 택배', value: 'kr.cupost' },
+                    { name: '한진택배', value: 'kr.hanjin' },
+                    { name: '로젠 택배', value: 'kr.logen' },
+			    )            
+                .setRequired(true)
+        )
+        .addNumberOption(option =>
+            option.setName('송장 번호')
+              .setDescription(
+                '송장번호를 입력해주세요'
+              )
+              .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        let user = await Tracking_number.findOne({user_id: id})
+        if (!user) {
+            user = new Tracking_number({
+                user_id: id,
+                org: interaction.options.get('택배사').value,
+                num: interaction.options.get('송장 번호').value,
+            })
+            await user.save()
+            let embed = new MessageEmbed()
+                .setTitle('송장 등록 완료!')
+                .setDescription('이제부터 "!송장"을 사용하여 송장을 조회할 수 있습니다.')
+                .setColor(0x00AE86)
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        } else {
+            await Tracking_number.findOneAndUpdate({ user_id: id }, { org: interaction.option.get("택배사").value, num: interaction.option.get("송장번호").value});
+            let embed = new MessageEmbed()
+                .setTitle('송장 수정 완료!')
+                .setDescription('이제부터 "!송장"을 사용하여 새 송장을 조회할 수 있습니다.')
+                .setColor(0x00AE86)
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+    }
+}
