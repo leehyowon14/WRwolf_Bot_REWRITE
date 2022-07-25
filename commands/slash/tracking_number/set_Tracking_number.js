3d9fdd1027d318cc17d5e64d2aa2967bdffedf83
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2022-07-25 15:56:09 +0900

    hotfix

--- commands/slash/tracking_number/set_Tracking_number.js
+++ commands/slash/tracking_number/set_Tracking_number.js
@@ -20,7 +20,7 @@ module.exports = {
                 .addChoice('로젠 택배', 'kr.logen')       
                 .setRequired(true)
         )
-        .addIntegerOption(option =>
+        .addNumberOption(option =>
             option.setName('송장번호')
               .setDescription(
                 '송장번호를 입력해주세요'
