635d55bdb04a310fcfb8212825149309df1d8cd3
leehyowon14 <lhwmessageonlyemail@gmail.com>
2023-08-04 13:54:19 +0900

    태그 필터 추가

--- modules/pixiv_keyword.js
+++ modules/pixiv_keyword.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
 
-module.exports = {
-    search: async (keywords, nsfw) => {
+module.exports = { //ふたなり
+    search: async (keywords, nsfw, forbidden_tags) => { //keywords: 검색할 키워드 (string || array), nsfw: nsfw 여부 (boolean), forbidden_tags: 금지 태그 (string || array)
         //https://www.pixiv.net/ajax/search/artworks/키워드?word=키워드&order=popular_d&mode=검색모드&p=1&s_mode=s_tag_full&type=all&lang=ko 검색모드: all/safe/r18
         if (keywords.length == 0) return false;
         if (typeof keywords == "string") keywords = [keywords]
+        if (typeof forbidden_tags == "string") forbidden_tags = [forbidden_tags]
         if (!Array.isArray(keywords)) return false;
 
         let req_url = `https://www.pixiv.net/ajax/search/artworks/${encodeURI(keywords.join(" "))}?word=${encodeURI(keywords.join(" "))}&order=popular_d&mode=safe&p=1&s_mode=s_tag_full&type=all&lang=ko`
@@ -23,39 +24,59 @@ module.exports = {
                 if (data.error) return false;
                 data = data.body
 
+                let include_forbidden_tags = false
                 let array = []
-                let obj = {
-                    id: 0,
-                    alt: '',
-                    url: '',
-                    createDate: ''
-                }
 
                 data.illustManga.data.forEach((object) => {
+                    // object.tags[3] = "ふたなり"
                     let date_split = object.createDate.split("T")
-                    obj.id = object.id
-                    obj.alt = object.alt
-                    obj.url = object.url
-                    obj.createDate = date_split[0] + " " + date_split[1].split("+")[0]
-                    array.push(obj)
+                    if (forbidden_tags.length != 0) {
+                        for (let tag of object.tags) {
+                            if (forbidden_tags.includes(tag)) {
+                                include_forbidden_tags = true;
+                                break;
+                            }
+                        }
+                        
+                        if (!include_forbidden_tags) array.push({ id: object.id, alt: object.alt, url: object.url, createDate: date_split[0] + " " + date_split[1].split("+")[0] })
+                        include_forbidden_tags = false;
+                    } else {
+                        array.push({ id: object.id, alt: object.alt, url: object.url, createDate: date_split[0] + " " + date_split[1].split("+")[0] })
+                    }
                 })
 
                 data.popular.recent.forEach((object) => {
                     let date_split = object.createDate.split("T")
-                    obj.id = object.id
-                    obj.alt = object.alt
-                    obj.url = object.url
-                    obj.createDate = date_split[0] + " " + date_split[1].split("+")[0]
-                    array.push(obj)
+                    if (forbidden_tags.length != 0) {
+                        for (let tag of object.tags) {
+                            if (forbidden_tags.includes(tag)) {
+                                include_forbidden_tags = true;
+                                break;
+                            }
+                        }
+                        
+                        if (!include_forbidden_tags) array.push({ id: object.id, alt: object.alt, url: object.url, createDate: date_split[0] + " " + date_split[1].split("+")[0] })
+                        include_forbidden_tags = false;
+                    } else {
+                        array.push({ id: object.id, alt: object.alt, url: object.url, createDate: date_split[0] + " " + date_split[1].split("+")[0] })
+                    }
                 })
 
                 data.popular.permanent.forEach((object) => {
                     let date_split = object.createDate.split("T")
-                    obj.id = object.id
-                    obj.alt = object.alt
-                    obj.url = object.url
-                    obj.createDate = date_split[0] + " " + date_split[1].split("+")[0]
-                    array.push(obj)
+                    if (forbidden_tags.length != 0) {
+                        for (let tag of object.tags) {
+                            if (forbidden_tags.includes(tag)) {
+                                include_forbidden_tags = true;
+                                break;
+                            }
+                        }
+                        
+                        if (!include_forbidden_tags) array.push({ id: object.id, alt: object.alt, url: object.url, createDate: date_split[0] + " " + date_split[1].split("+")[0] })
+                        include_forbidden_tags = false;
+                    } else {
+                        array.push({ id: object.id, alt: object.alt, url: object.url, createDate: date_split[0] + " " + date_split[1].split("+")[0] })
+                    }
                 })
                 return array
             })
@@ -64,5 +85,5 @@ module.exports = {
                 return false;
             });
         return res
-    } 
+    }
 }
