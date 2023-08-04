e4a073f69b8a4d682b8446521d98d6fbe6757ca6
leehyowon14 <lhwmessageonlyemail@gmail.com>
2023-08-04 14:03:48 +0900

    인자 기본값 할당

--- modules/pixiv_keyword.js
+++ modules/pixiv_keyword.js
@@ -1,12 +1,17 @@
 const axios = require("axios");
 
 module.exports = { //ふたなり
-    search: async (keywords, nsfw, forbidden_tags) => { //keywords: 검색할 키워드 (string || array), nsfw: nsfw 여부 (boolean), forbidden_tags: 금지 태그 (string || array)
+    search: async (keywords = [], nsfw=false, forbidden_tags=[]) => { //keywords: 검색할 키워드 (string || array), nsfw: nsfw 여부 (boolean), forbidden_tags: 금지 태그 (string || array)
         //https://www.pixiv.net/ajax/search/artworks/키워드?word=키워드&order=popular_d&mode=검색모드&p=1&s_mode=s_tag_full&type=all&lang=ko 검색모드: all/safe/r18
         if (keywords.length == 0) return false;
         if (typeof keywords == "string") keywords = [keywords]
-        if (typeof forbidden_tags == "string") forbidden_tags = [forbidden_tags]
         if (!Array.isArray(keywords)) return false;
+
+        let include_forbidden_tags, isFilterActivated = false
+        if (forbidden_tags.length != 0) {
+            if (typeof forbidden_tags == "string") forbidden_tags = [forbidden_tags]
+            isFilterActivated = true
+        }
 
         let req_url = `https://www.pixiv.net/ajax/search/artworks/${encodeURI(keywords.join(" "))}?word=${encodeURI(keywords.join(" "))}&order=popular_d&mode=safe&p=1&s_mode=s_tag_full&type=all&lang=ko`
         if (nsfw) {
@@ -24,13 +29,13 @@ module.exports = { //ふたなり
                 if (data.error) return false;
                 data = data.body
 
-                let include_forbidden_tags = false
+                
                 let array = []
 
                 data.illustManga.data.forEach((object) => {
                     // object.tags[3] = "ふたなり"
                     let date_split = object.createDate.split("T")
-                    if (forbidden_tags.length != 0) {
+                    if (isFilterActivated) {
                         for (let tag of object.tags) {
                             if (forbidden_tags.includes(tag)) {
                                 include_forbidden_tags = true;
@@ -47,7 +52,7 @@ module.exports = { //ふたなり
 
                 data.popular.recent.forEach((object) => {
                     let date_split = object.createDate.split("T")
-                    if (forbidden_tags.length != 0) {
+                    if (isFilterActivated) {
                         for (let tag of object.tags) {
                             if (forbidden_tags.includes(tag)) {
                                 include_forbidden_tags = true;
@@ -64,7 +69,7 @@ module.exports = { //ふたなり
 
                 data.popular.permanent.forEach((object) => {
                     let date_split = object.createDate.split("T")
-                    if (forbidden_tags.length != 0) {
+                    if (isFilterActivated) {
                         for (let tag of object.tags) {
                             if (forbidden_tags.includes(tag)) {
                                 include_forbidden_tags = true;
