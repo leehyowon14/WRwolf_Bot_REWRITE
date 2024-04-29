76f835b94a241886e2dbd02641cc71ed67209587
leehyowon14 <kingtv347@gmail.com>
2024-04-30 00:42:46 +0900

    한강 수온 API 변경

--- commands/non-slash/misc/han.js
+++ commands/non-slash/misc/han.js
@@ -12,14 +12,25 @@ module.exports = {
         if (args[0]) {
             return;
         }
-        axios.get(`http://hangang.dkserver.wo.tc`)
+        axios.get(`https://api.hangang.life`)
             .then(function (response) {
                 if (response.status == 200) {
                     let river = response.data
+                    if (river.STATUS == "OK") {
+                        river = river.DATAs.DATA.HANGANG.노량진
+                    } else {
+                        let embed = new EmbedBuilder()
+                            .setColor('#ED4245')
+                            .setAuthor({ name :`에러!` })
+                            .addFields({name: "Error", value: error})
+                            .setTimestamp()
+                            .setFooter({ text: 'Developed by sG.wolf' })
+                        return message.channel.send({ embeds: [embed] })
+                    }
                     let embed = new EmbedBuilder()
                         .setColor('#4fe8a3')
                         .setTitle('한강 수온')
-                        .addFields({name: ':droplet: ' + river.temp, value: '측정 시각: ' + river.time, inline: true})
+                        .addFields({name: ':droplet: ' + river.TEMP, value: '측정 시각: ' + river.LAST_UPDATE, inline: true})
                     return message.reply({ embeds: [embed] })
                 }
             })
